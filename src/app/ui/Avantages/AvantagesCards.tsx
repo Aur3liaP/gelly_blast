eb431675762d07516d14f1e4016a89178931485a
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-30 19:38:52 +0200

    Resolve color bug

--- src/app/ui/Avantages/AvantagesCards.tsx
+++ src/app/ui/Avantages/AvantagesCards.tsx
@@ -9,7 +9,8 @@ export default function AvantagesCards({ titre, texte, color = "text-black", bgC
   return (
     <div className="w-80 h-96 bg-white shadow-xl/20 rounded-lg border border-gray-200 flex-shrink-0">
       <div 
-        className={`bg-${bgColor} ${color} font-medium flex flex-col text-center justify-center gap-4 w-72 h-72 mt-4 mx-auto rounded-lg p-4`}
+        className={`font-medium flex flex-col text-center justify-center gap-4 w-72 h-72 mt-4 mx-auto rounded-lg p-4 ${color}`}
+        style={{ backgroundColor: `var(--${bgColor})` }}
       >
         <h3 className="text-2xl font-semibold">{titre}</h3>
         <p className="text-base leading-relaxed">
